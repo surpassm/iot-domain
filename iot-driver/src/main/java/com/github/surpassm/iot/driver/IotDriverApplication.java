d9273b2619acca1b6b7d42151e08cae6a7f914fc
lustres <lustres@sina.com>
2020-10-17 14:36:52 +0800

    新增驱动模块，重新整合项目结构

---
+++ iot-driver/src/main/java/com/github/surpassm/iot/driver/IotDriverApplication.java
@@ -0,0 +1,16 @@
+package com.github.surpassm.iot.driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author AOC
+ */
+@SpringBootApplication
+public class IotDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotDriverApplication.class, args);
+    }
+
+}
---
+++ iot-driver/src/test/java/com/github/surpassm/iot/driver/IotDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.surpassm.iot.driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotDriverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
