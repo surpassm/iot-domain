b75a484cccdb9bdf7c32cd7641ae549a02565d4f
lustres <lustres@sina.com>
2020-11-09 17:09:43 +0800

    新增启动入口iot-start-center

---
+++ iot-start-center/iot-start-auth/src/main/java/com/github/surpassm/iot/start/auth/IotStartAuthApplication.java
@@ -0,0 +1,16 @@
+package com.github.surpassm.iot.start.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author AOC
+ */
+@SpringBootApplication
+public class IotStartAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotStartAuthApplication.class, args);
+    }
+
+}
